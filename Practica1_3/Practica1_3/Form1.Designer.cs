4abe970ef7a4da32573c206668ed4954acf18fda
Sergio <sergioseguradura@gmail.com>
2022-05-05 15:29:46 +0200

    cambios 2

--- Practica1_3/Practica1_3/Form1.Designer.cs
+++ Practica1_3/Practica1_3/Form1.Designer.cs
@@ -31,31 +31,75 @@ namespace Practica1_3
         {
             this.label1 = new System.Windows.Forms.Label();
             this.tnombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tapellidos = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ttelf = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(93, 87);
+            this.label1.Location = new System.Drawing.Point(83, 107);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.Size = new System.Drawing.Size(56, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre";
             // 
             // tnombre
             // 
-            this.tnombre.Location = new System.Drawing.Point(188, 87);
+            this.tnombre.Location = new System.Drawing.Point(210, 107);
+            this.tnombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tnombre.Name = "tnombre";
-            this.tnombre.Size = new System.Drawing.Size(100, 20);
+            this.tnombre.Size = new System.Drawing.Size(132, 22);
             this.tnombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(413, 110);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Apellidos";
+            // 
+            // tapellidos
+            // 
+            this.tapellidos.Location = new System.Drawing.Point(514, 107);
+            this.tapellidos.Name = "tapellidos";
+            this.tapellidos.Size = new System.Drawing.Size(132, 22);
+            this.tapellidos.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(697, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Teléfono";
+            // 
+            // ttelf
+            // 
+            this.ttelf.Location = new System.Drawing.Point(796, 104);
+            this.ttelf.Name = "ttelf";
+            this.ttelf.Size = new System.Drawing.Size(132, 22);
+            this.ttelf.TabIndex = 5;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.ttelf);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tapellidos);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.tnombre);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -67,6 +111,10 @@ namespace Practica1_3
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tnombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tapellidos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ttelf;
     }
 }
 
