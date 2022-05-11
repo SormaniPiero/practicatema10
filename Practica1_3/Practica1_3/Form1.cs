5e2f5fc5907d1f637b085208ef8c520b2b6d794d
SormaniPiero <sormanipiero@gmail.com>
2022-05-11 19:55:03 +0200

    Añadi codigo al proyecto

--- Practica1_3/Practica1_3/Form1.cs
+++ Practica1_3/Practica1_3/Form1.cs
@@ -21,5 +21,10 @@ namespace Practica1_3
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Aaron");
+        }
     }
 }
