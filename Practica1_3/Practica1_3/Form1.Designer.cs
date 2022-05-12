02db14692bdcbf5d47303ad64269df871b7c7f34
SormaniPiero <sormanipiero@gmail.com>
2022-05-12 15:45:37 +0200

    Sincronización

--- Practica1_3/Practica1_3/Form1.Designer.cs
+++ Practica1_3/Practica1_3/Form1.Designer.cs
@@ -90,7 +90,7 @@ namespace Practica1_3
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(615, 119);
+            this.button1.Location = new System.Drawing.Point(616, 119);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(120, 21);
             this.button1.TabIndex = 6;
